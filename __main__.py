b1a14171eeaea1e0fc9b1cf0859e4fba157fbad6
christopherkopic <christopherkopic@web.de>
2019-06-12 21:32:02 +0200

    Comments, better variable / functions names, less hard coding, seperate dithering function

--- __main__.py
+++ __main__.py
@@ -5,24 +5,33 @@ from random import randint
 import os
 import textwrap
 
-file_txt = "Icons.h"
+data_folder = "testdata"
+file_format = ".bmp"
+output_folder = "testoutput"
+output_file = "Icons.h"
 
 def writeImg(file, pixels, sizeX, sizeY, name):
+    """Writes picture byte array into output file"""
+    #Image Dimensions
     file.write("const unsigned int {0}_X = {1};\n".format(name, sizeX))
     file.write("const unsigned int {0}_Y = {1};\n".format(name, sizeY))
 
+    #Array Header
     file.write("const unsigned char {0}[] PROGMEM =\n".format(name))
     file.write("{\n")
 
+    #Array
     file.write("\t0X{:02X}".format(pixels.pop(0)))
     for pixel in pixels:
         file.write(", 0X{:02X}".format(pixel))
     file.write("\n")
 
+    #Array Footer
     file.write("};\n")
     file.write("\n")
 
 def writeFileHead(file, filename):
+    """Header for output file"""
     name = Path(filename).stem
     file.write("#ifndef _{0}_H_\n".format(name))
     file.write("#define _{0}_H_\n".format(name))
@@ -35,13 +44,15 @@ def writeFileHead(file, filename):
     file.write("\n")
 
 def writeFileFoot(file):
+    """Footer for output file"""
     file.write("#endif\n")
 
 def grouper(n, iterable, padvalue=None):
     "grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
     return list(zip_longest(*[iter(iterable)]*n, fillvalue=padvalue))
 
-def encodePixels(pixels, sizeX):
+def encodeBinary(pixels, sizeX):
+    """Encodes a bitmap as a string of binary values inte an array of bytes for arduino image processing"""
     rows = grouper(sizeX, pixels, '0');
     data = []
     for row in rows:
@@ -56,39 +67,45 @@ def encodePixels(pixels, sizeX):
     return data_encoded
 
 def convertToBinary(pixels):
+    """Converts an array of pixel values into a string of binary values through dithering"""
     binstring = ""
     i = 0
     for pixel in pixels:
-        if(pixel < randint(0,255)):
-            binstring += "0"
-        else:
-            binstring += "1"
-
+        binstring += dither_random(pixel, 255)
     return binstring
+
+def dither_random(value, maxdepth):
+    """Dithers pixels by comparing their value with a random value within their color range"""
+    if(value < randint(0, maxdepth)):
+        return "0"
+    else:
+        return "1"
 
 def main():
     #Load Textfile
-    out_folder = Path("testoutput")
-    txt = open(out_folder / file_txt, "w+")
-    writeFileHead(txt, file_txt)
+    out_path = Path(output_folder)
+    txt = open(out_path / output_file, "w+")
+    writeFileHead(txt, output_file)
 
     #All bmp files as list
-    pictures = os.listdir("testdata")
-    pictures_bmp = [i for i in pictures if i.endswith('.bmp')]
-    picture_folder = Path("testdata")
+    import_files = os.listdir(data_folder)
+    pictures = [i for i in import_files if i.endswith(file_format)]
+    picture_path = Path(data_folder)
 
-    for bmp in pictures_bmp:
+    for picture in pictures:
         #Load Image
-        img = Image.open(picture_folder / bmp)
+        img = Image.open(picture_path / picture)
         img = img.convert('L')
         pixels = list(img.getdata())
         sizeX = img.size[0];
         sizeY = img.size[1];
-        name = Path(bmp).stem
+        name = Path(picture).stem
 
+        #Encode into bytearray
         binpixels = convertToBinary(pixels)
-        bytearray = encodePixels(binpixels, sizeX)
+        bytearray = encodeBinary(binpixels, sizeX)
 
+        #Write data into output file
         writeImg(txt, bytearray, sizeX, sizeY, name)
 
     writeFileFoot(txt)
